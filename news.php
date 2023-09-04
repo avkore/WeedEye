2e6c3dcaec199d192589a3135f1933bdb32f13ac
avko <dachi.avkopashvili.1@btu.edu.ge>
2023-09-04 22:10:45 +0400

    updated blog page

--- news.php
+++ news.php
@@ -1,30 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+	.blogname {
+		height: 50px;
+	}
+</style>
+
 <?php
-    include('parts/head.php');
-    ?>
+include('parts/head.php');
+
+function selectFirstCharacters($inputString, $charCount)
+{
+	// If the string length is less than or equal to the limit, return the original string
+	if (mb_strlen($inputString) <= $charCount) {
+		return $inputString;
+	}
+
+	// Otherwise, slice the string to get the first $charCount characters
+	$selectedCharacters = mb_substr($inputString, 0, $charCount);
+
+	return $selectedCharacters;
+}
+?>
+
 <body>
-	
+
 	<!--PreLoader-->
-    <div class="loader">
-        <div class="loader-inner">
-            <div class="circle"></div>
-        </div>
-    </div>
-    <!--PreLoader Ends-->
-	
+	<div class="loader">
+		<div class="loader-inner">
+			<div class="circle"></div>
+		</div>
+	</div>
+	<!--PreLoader Ends-->
+
 	<!-- header -->
 	<?php
-    include('parts/header.php');
-    ?>
+	include('parts/header.php');
+	?>
 	<!-- end header -->
 
 	<!-- search area -->
 	<?php
-    include('parts/search.php');
-    ?>
+	include('parts/search.php');
+	?>
 	<!-- end search arewa -->
-	
+
 	<!-- breadcrumb-section -->
 	<div class="breadcrumb-section breadcrumb-bg">
 		<div class="container">
@@ -44,96 +64,36 @@
 	<div class="latest-news mt-150 mb-150">
 		<div class="container">
 			<div class="row">
-			<div class="col-lg-4 col-md-6">
-					<div class="single-latest-news">
-						<a href="single-news1.php">
-							<div class="latest-news-bg news-bg-1"></div>
-						</a>
-						<div class="news-text-box">
-							<h3><a href="single-news1.php">Pennsylvania Police Make More Than 20,000 Pot Arrests During The Pandemic</a></h3>
-							<p class="blog-meta">
-								<span class="author"><i class="fas fa-user"></i> Admin</span>
-								<span class="date"><i class="fas fa-calendar"></i> 17 February, 2023</span>
-							</p>
-							<p class="excerpt" maxlength="12">Throughout the past year, there has been much talk about how to make things more equitable for people of color and marginalized communities, and a big part of that is providing equity programs...</p>
-							<a href="single-news1.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
+				<?php
+				$query1 = "SELECT * FROM blogs";
+				$result1 = mysqli_query($conn, $query1);
+				while ($row = mysqli_fetch_array($result1)) {
+				?>
+					<div class="col-lg-4 col-md-6">
+						<div class="single-latest-news">
+							<a href="blogView.php?id=<?php echo $row[0]; ?>"><img class="latest-news-bg" id="MediaImg" width="100%" height="180" src="AdminPanel/dist/blogImageView.php?image_id=<?php echo $row[0]; ?> "></a>
+							<div class="news-text-box">
+								<div class="blogname">
+									<h3><a href="single-news1.php"><?php echo $row['name']; ?></a></h3>
+								</div>
+								<p class="blog-meta">
+									<span class="author"><i class="fas fa-user"></i> Admin</span>
+									<span class="date"><i class="fas fa-calendar"></i> <?php $originalDate = $row['date'];
+																						$formattedDate = date('j F, Y', strtotime($originalDate));
+																						echo $formattedDate; ?></span>
+								</p>
+								<p class="excerpt" maxlength="12"><?php
+																	$inputString = $row['description'];
+																	$charCount = 220; // Change this to the desired number of characters
+																	$selectedCharacters = selectFirstCharacters($inputString, $charCount);
+																	echo $selectedCharacters;
+																	?>...</p>
+								<a href="single-news1.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
+							</div>
+
 						</div>
 					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-latest-news">
-						<a href="single-news2.php">
-							<div class="latest-news-bg news-bg-2"></div>
-						</a>
-						<div class="news-text-box">
-							<h3><a href="single-news2.php">Blackberry Moonrock Cocktail</a></h3>
-							<p class="blog-meta">
-								<span class="author"><i class="fas fa-user"></i> Admin</span>
-								<span class="date"><i class="fas fa-calendar"></i> 19 February, 2019</span>
-							</p>
-							<p class="excerpt">If you're looking for a sophisticated and unique cocktail to impress your guests, look no further than the Blackberry Moonrock. This drink combines the tart sweetness of blackberries with the smoothness of bourbon and the herbal notes of Chartreuse for a drink that is truly out of this world.</p>
-							<a href="single-news2.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
-					<div class="single-latest-news">
-						<a href="single-news3.php">
-							<div class="latest-news-bg news-bg-3"></div>
-						</a>
-						<div class="news-text-box">
-							<h3><a href="single-news3.php">Cannabis Varieties</a></h3>
-							<p class="blog-meta">
-								<span class="author"><i class="fas fa-user"></i> Admin</span>
-								<span class="date"><i class="fas fa-calendar"></i> 23 January, 2023</span>
-							</p>
-							<p class="excerpt">For many years regardless of origin all cannabis was classified as Cannabis Sativa and still remains so under international law. Thanks to the work of scientists who have studied cannabis over the past 30+ years we know that cannabis can be further classified into 4 distinct categories or species; Cannabis Sativa Cannabis Indica ...</p>
-							<a href="single-news3.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-latest-news">
-						<a href="single-news4.php"><div class="latest-news-bg news-bg-4"></div></a>
-						<div class="news-text-box">
-							<h3><a href="single-news4.php">How To Store Cannabis Seeds</a></h3>
-							<p class="blog-meta">
-								<span class="author"><i class="fas fa-user"></i> Admin</span>
-								<span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
-							</p>
-							<p class="excerpt">Cannabis seeds are the starting point for growing your own cannabis plants. They are also valuable commodities, as they can be used for breeding and creating new strains. Proper storage of cannabis seeds is...</p>
-							<a href="single-news4.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-latest-news">
-						<a href="single-news5.php"><div class="latest-news-bg news-bg-5"></div></a>
-						<div class="news-text-box">
-							<h3><a href="single-news5.php">How To Germinate Seeds</a></h3>
-							<p class="blog-meta">
-								<span class="author"><i class="fas fa-user"></i> Admin</span>
-								<span class="date"><i class="fas fa-calendar"></i> 21 January, 2023</span>
-							</p>
-							<p class="excerpt">Growing plants from seeds can be a rewarding and cost-effective way to start your garden. Germinating seeds is the first step in this process, and it's essential to get it right to ensure successful plant growth. Here are some tips on how to germinate seeds...</p>
-							<a href="single-news5.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-latest-news">
-						<a href="single-news6.php"><div class="latest-news-bg news-bg-6"></div></a>
-						<div class="news-text-box">
-							<h3><a href="single-news6.php">Life Cycle of Cannabis</a></h3>
-							<p class="blog-meta">
-								<span class="author"><i class="fas fa-user"></i> Admin</span>
-								<span class="date"><i class="fas fa-calendar"></i> 17 January, 2023</span>
-							</p>
-							<p class="excerpt">Cannabis, also known as marijuana, is a plant that has been used for medicinal and recreational purposes for thousands of years. Like all plants, cannabis has a life cycle that starts with a seed and ends with the production of more seeds. In this blog...</p>
-							<a href="single-news6.php" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-						</div>
-					</div>
-				</div>
+				<?php  } ?>
 			</div>
 		</div>
 	</div>
@@ -150,11 +110,12 @@
 	include('parts/footer.php');
 	?>
 	<!-- end footer -->
-	
+
 	<!-- scripts -->
 	<?php
 	include('parts/script.php');
 	?>
 
 </body>
+
 </html>
